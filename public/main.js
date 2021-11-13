a7a289ff89eb26643abab13cac56c9bcb7363cba
Jonathan Graziani <jpgraziani@mail.com>
2021-11-13 15:08:06 -0700

    updated nav

--- public/main.js
+++ public/main.js
@@ -6,8 +6,15 @@
 // 	menu.classList.toggle("hidden");
 // });
 
-const btn = document.querySelector('button.menu-button');
-  const menu = document.querySelector(".mobile-menu");
-  btn.addEventListener("click", () => {
-      menu.classList.toggle("hidden");
-})
+// const btn = document.querySelector('button.menu-button');
+//   const menu = document.querySelector(".mobile-menu");
+//   btn.addEventListener("click", () => {
+//       menu.classList.toggle("hidden");
+// })
+
+document.getElementById("hamburger").onclick = function toggleMenu() {
+  const navToggle = document.getElementsByClassName("toggle");
+  for (let i = 0; i < navToggle.length; i++) {
+    navToggle.item(i).classList.toggle("hidden");
+  }
+};
