ea0459e4726346b19878a299ebec6469d9d2cf9b
Jonathan Graziani <jpgraziani@mail.com>
2021-11-28 15:24:36 -0700

    updated css styles

--- public/main.js
+++ public/main.js
@@ -1,20 +1,6 @@
-// const btn = document.querySelector("button.mobile-menu-button");
-// const menu = document.querySelector(".mobile-menu");
+const btn = document.querySelector("button.mobile-menu-button");
+				const menu = document.querySelector(".mobile-menu");
 
-// // Add Event Listeners
-// btn.addEventListener("click", () => {
-// 	menu.classList.toggle("hidden");
-// });
-
-// const btn = document.querySelector('button.menu-button');
-//   const menu = document.querySelector(".mobile-menu");
-//   btn.addEventListener("click", () => {
-//       menu.classList.toggle("hidden");
-// })
-
-document.getElementById("hamburger").onclick = function toggleMenu() {
-  const navToggle = document.getElementsByClassName("toggle");
-  for (let i = 0; i < navToggle.length; i++) {
-    navToggle.item(i).classList.toggle("hidden");
-  }
-};
+				btn.addEventListener("click", () => {
+					menu.classList.toggle("hidden");
+				});
